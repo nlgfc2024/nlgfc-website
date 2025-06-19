958ca2783236986ffc155e662527bc7ff6178b7c
AndyoneMphiningo <157719084+AndyoneMphiningo@users.noreply.github.com>
2025-06-19 15:57:28 +0200

    update index

--- index.php
+++ index.php
@@ -207,7 +207,7 @@
 
         <div class="logo" style="display: flex; align-items: center; gap: 6px;">
   <img src="assets/img/nlgfc-logo.png" alt="NLGFC Logo" style="height: 80px;">
-  <span>NLGFC <strong>2025</strong></span>
+  NLGFC<span>2025</span>
 </div>
 
 
