ab826f0f335154a4fc64754e000250830f1cb61a
AndyoneMphiningo <157719084+AndyoneMphiningo@users.noreply.github.com>
2025-06-19 12:10:54 +0200

    update index

--- index.php
+++ index.php
@@ -207,7 +207,7 @@
                 <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
                 <polyline points="9 22 9 12 15 12 15 22"></polyline>
             </svg>
-            NLGFC<span>2025</span>
+            NLGFC WEBSITE RE-DEVELOPMENT<span>2025</span>
         </div>
 
         <h1>Something Amazing Is Coming Soon</h1>
