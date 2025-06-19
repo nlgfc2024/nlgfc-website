12e73de780af1c7b303073ec0d28d451e3971507
AndyoneMphiningo <157719084+AndyoneMphiningo@users.noreply.github.com>
2025-06-19 11:56:22 +0200

    update index tittle

--- index.php
+++ index.php
@@ -205,7 +205,7 @@
                 <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
                 <polyline points="9 22 9 12 15 12 15 22"></polyline>
             </svg>
-            Brand<span>X</span>
+            NLGFC<span>2025</span>
         </div>
         
         <h1>Something Amazing Is Coming Soon</h1>
