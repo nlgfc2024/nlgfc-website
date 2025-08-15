67890e52fd32883dd5a75fce30b8c9440d425472
8aggie <38732712+8aggie@users.noreply.github.com>
2025-08-15 15:55:45 +0200

    update Nav

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -296,7 +296,7 @@
                     <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"></path>
                     </svg>
-                    <li><NuxtLink to="/projects/currentProjects#usr"  class="text-gray-600 hover:text-blue-600">Urban Malawi Social Registry</NuxtLink></li>
+                    <li><NuxtLink to="/projects/currentProjects#usr"  class="text-gray-600 hover:text-blue-600">Urban Social Registry</NuxtLink></li>
                   </a>
                 </li>
                 <li>
