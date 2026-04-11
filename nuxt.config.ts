e0c26242ae37d7c00658090cb9660017a749ed7d
precious manyoni <vbhnx83w@boma.gov.mw>
2026-04-11 13:14:19 +0200

    modified nuxt.config.ts and deploy-production.yml files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,6 +26,10 @@ export default defineNuxtConfig({
     }
   },
   // Configuration for subdirectory deployment and app settings
+  experimental: {
+    appManifest: false
+  },
+
   app: {
     baseURL: '/nlgfc-website/',
     buildAssetsDir: '/_nuxt/',
